3e75eb3c036a3380c78a7000ab94a55ee7dda96c
Bhavesh chaubey <48830608+bhaveshcha@users.noreply.github.com>
2023-11-19 19:40:12 +0530

    go

--- script.js
+++ script.js
@@ -107,81 +107,71 @@
 
 
 
-  async function updateSpotifyInfo() {
-    const accessToken = 'YOUR_ACCESS_TOKEN'; // Obtain this via OAuth
+//   async function updateSpotifyInfo() {
+//     const accessToken = 'YOUR_ACCESS_TOKEN'; // Obtain this via OAuth
 
-    try {
-        const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-            headers: { 'Authorization': 'Bearer ' + accessToken }
-        });
+//     try {
+//         const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+//             headers: { 'Authorization': 'Bearer ' + accessToken }
+//         });
 
-        if (response.ok) {
-            const data = await response.json();
-            document.getElementById('song-title').textContent = data.item.name;
-            document.getElementById('artist-name').textContent = data.item.artists.map(artist => artist.name).join(', ');
-            document.getElementById('spotify-song-link').href = data.item.external_urls.spotify;
-        } else {
-            console.log('Spotify API request failed:', response.status);
-            // Handle errors or cases where no song is playing
-        }
-    } catch (error) {
-        console.error('Error fetching Spotify data:', error);
-    }
+//         if (response.ok) {
+//             const data = await response.json();
+//             document.getElementById('song-title').textContent = data.item.name;
+//             document.getElementById('artist-name').textContent = data.item.artists.map(artist => artist.name).join(', ');
+//             document.getElementById('spotify-song-link').href = data.item.external_urls.spotify;
+//         } else {
+//             console.log('Spotify API request failed:', response.status);
+//             // Handle errors or cases where no song is playing
+//         }
+//     } catch (error) {
+//         console.error('Error fetching Spotify data:', error);
+//     }
+// }
+
+// // Call the function to update the song info
+// updateSpotifyInfo();
+
+
+
+
+const clientId = "d7d2615e130742fd8e3eed1f2e8d3414"; // Replace with your client ID
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
 }
 
-// Call the function to update the song info
-updateSpotifyInfo();
+async function redirectToAuthCodeFlow(clientId) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId, code) {
+  // TODO: Get access token for code
+}
+
+async function fetchProfile(token) {
+    // TODO: Call Web API
+}
+
+function populateUI(profile) {
+    // TODO: Update UI with profile data
+}
 
 
 
 
-const request = require('request'); // You may need to install the 'request' package
 
-const client_id = 'd7d2615e130742fd8e3eed1f2e8d3414';
-const client_secret = 'ad5c7dbbacaa4f46ab2f75d3bdaaab73';
-const redirect_uri = 'https://bhaveshcha.github.io/Portfolio/';
-const code = 'AQB4T2s6eSvLciRJAdp8mG68CEVZ3uixRUBK8w8PQ7vwjPwLNt2ccDlBcaX0DpKP5qEVPxR7cE8uhMj7TPJRThDszumXX-USjBZtoXltl2a7Lq9ZyC9igpKPrJx_KHhKdzuFAvLbt9yuz3l5TrC1pXmSRwmmuAf8jTbhyA0_8EXsbTJR7TzmkSu-UT2H5qFoTukVXhqKBr5XXS0hTT8Q3taQRCOJtbM'; // Replace with your actual code
 
-const authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  form: {
-    code: code,
-    redirect_uri: redirect_uri,
-    grant_type: 'authorization_code'
-  },
-  json: true
-};
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const access_token = body.access_token;
-    const refresh_token = body.refresh_token;
-    // Use the access token to access the Spotify Web API
-    // Save the refresh token for later use
-    console.log('Access Token:', access_token);
-  } else {
-    console.error('Error:', body);
-  }
-});
-const client_secret = 'ad5c7dbbacaa4f46ab2f75d3bdaaab73';
-const redirect_uri = 'https://bhaveshcha.github.io/Portfolio/';
-const code = 'AQB4T2s6eSvLciRJAdp8mG68CEVZ3uixRUBK8w8PQ7vwjPwLNt2ccDlBcaX0DpKP5qEVPxR7cE8uhMj7TPJRThDszumXX-USjBZtoXltl2a7Lq9ZyC9igpKPrJx_KHhKdzuFAvLbt9yuz3l5TrC1pXmSRwmmuAf8jTbhyA0_8EXsbTJR7TzmkSu-UT2H5qFoTukVXhqKBr5XXS0hTT8Q3taQRCOJtbM';
 
-const response = await fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
-  },
-  body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}`
-});
 
-const data = await response.json();
-console.log(data);
+
+
 
 
 
